6d72c2cef15e50bec177a4e80fa7b8240ee74213
John Bryant <john.r.bryant@gmail.com>
2022-12-20 16:08:10 +0700

    added posterior

---
+++ R/add_post_terms.R
@@ -0,0 +1,21 @@
+
+## add_post_terms <- function(terms, template) {
+##     nms_terms <- get_nms_terms(terms)
+##     nms_template <- names(dimnames(template))
+##     for (nm in nms_terms) {
+##         i_tme
+##     if (!all(setdiff(nms_terms, "zero") %in%
+    
+    
+
+## svd_lograte <- function(dim_target, term, margins_term) {
+##     ndim <- length(dim_target)
+##     i_dim <- rep(0, times = n_dim)
+##     for (j in seq_len(ndim)) {
+##         if (i_dim[j] == dim_target[j] - 1L)
+##             if j 
+##     n <- prod(dim_target)
+##     for (i in seq_len(n)) {
+        
+##     n <- cumprod(dim_target)
+    
