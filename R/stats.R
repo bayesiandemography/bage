b6831831e63f096104657fd65a06d87aee2ae999
John Bryant <john.r.bryant@gmail.com>
2023-01-07 07:01:45 +0700

    working on outputs

--- R/stats.R
+++ R/stats.R
@@ -1,16 +1,6 @@
 
-logit <- function(p) {
-    log(p) - log(1 - p)
-}
 
-invlogit <- function(x) {
-    is_x_pos <- x > 0
-    ans <- double(length = x)
-    exp_neg_x <- exp(-1 * x[is_x_pos])
-    exp_x <- exp(x[!is_x_pos])
-    ans[is_x_pos] <- 1 / (1 + exp_neg_x)
-    ans[!is_x_pos] <- exp_x / (1 + exp_x)
-    ans
-}
-               
+
+                      
+             
     
