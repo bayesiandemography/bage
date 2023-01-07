d2d6c5e336908a2dfa4d7b5e737e25bccf2a1c9c
John Bryant <john.r.bryant@gmail.com>
2023-01-07 07:01:45 +0700

    working on outputs

--- R/make-outputs.R
+++ R/make-outputs.R
@@ -1,4 +1,18 @@
 
+
+get_inv_transform <- function(nm_distn) {
+    if (nm_distn == "pois")
+        exp
+    else if (nm_distn == "binom")
+        function(x) if_else(x > 0, 1 / (1 + exp(-x)), exp(x) / (1 + exp(x)))
+    else if (nm_disn == "norm")
+        function(x) x
+    else
+        stop(gettextf("invalid value for 'nm_distn' : %s",
+                      nm_distn),
+             call. = FALSE)
+}
+
 ## HAS_TESTS
 #' Extract point estimates for terms
 #'
@@ -56,25 +70,33 @@ make_linear_pred_sd <- function(mod) {
 
 make_fitted_point <- function(mod) {
     nm_distn <- mod$nm_distn
+    inv_transform <- get_inv_transform(nm_distn)
     linear_pred_mean <- make_linear_pred_mean(mod)
-    if (nm_distn == "pois")
-        exp(linear_pred_mean)
-    else if (nm_distn == "binom")
-        invlogit(linear_pred_mean)
-    else
-        linear_pred_mean
+    inv_transform(linear_pred_mean)
 }
 
+
+#' Make lower and upper limits of credible interval
+#'
+#' @param mod Fitted 'bage_mod' object
+#' @param interval p(upper) - p(lower)
+#'
+#' @returns Named list
+#'
+#' @noRd
 make_lower_upper <- function(mod, interval) {
-     nm_distn <- mod$nm_distn
-    linear_pred_mean <- make_linear_pred_mean(mod)
-    if (nm_distn == "pois")
-        exp(linear_pred_mean)
-    else if (nm_distn == "binom")
-        invlogit(linear_pred_mean)
-    else
-        linear_pred_mean
-}   
+    alpha <- (1 - interval) / 2
+    nm_distn <- mod$nm_distn
+    mean <- make_linear_pred_mean(mod)
+    sd <- make_linear_pred_sd(mod)
+    lower_trans <- pnorm(alpha, mean = mean, sd = sd)
+    upper_trans <- pnorm(1 - alpha, mean = mean, sd = sd)
+    inv_transform <- get_inv_transform(nm_distn)
+    lower <- inv_transform(lower_trans)
+    upper <- inv_transform(upper_trans)
+    list(lower = lower,
+         upper = upper)
+}
     
 
 
