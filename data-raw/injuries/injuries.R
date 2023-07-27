3fd1eb0407e676a9d71b4be57f4838473ea4227c
John Bryant <john.r.bryant@gmail.com>
2023-07-28 10:43:07 +1200

    tidied data-raw

--- data-raw/injuries/injuries.R
+++ data-raw/injuries/injuries.R
@@ -3,9 +3,14 @@ library(readr)
 library(dplyr)
 library(tidyr)
 library(poputils)
+library(command)
 
-injuries_all <- read_csv("injuries/TABLECODE7935_Data.csv.gz",
-                         col_types = "i") %>%
+cmd_assign(.injuries_raw = "injuries/TABLECODE7935_Data.csv.gz",
+           .popn_maori_raw = "injuries/DPE479901_20230101_021445_73.csv.gz",
+           .popn_total_raw = "injuries/DPE403903_20230101_021709_80.csv.gz",
+           .out = "../data/injuries.rda")
+
+injuries_all <- read_csv(.injuries_raw, col_types = "i") %>%
     select(age = "Age group",
            sex = Sex,
            ethnicity = Ethnicity,
@@ -14,7 +19,7 @@ injuries_all <- read_csv("injuries/TABLECODE7935_Data.csv.gz",
     mutate(age = if_else(age %in% c("60 - 89 years", "90 years and over"),
                          "60+",
                          age)) %>%
-    mutate(age = clean_age(age)) %>%
+    mutate(age = reformat_age(age)) %>%
     count(age, sex, ethnicity, year, wt = count, name = "injuries") %>%
     complete(age, sex, ethnicity, year, fill = list(injuries = 0L))
     
@@ -23,7 +28,7 @@ col_names_popn <- c("year",
                     paste(rep(c("Male", "Female"), each = length(ages)),
                         ages))
 
-popn_maori <- read_csv("injuries/DPE479901_20230101_021445_73.csv.gz",
+popn_maori <- read_csv(.popn_maori_raw,
                        skip = 3,
                        col_names = col_names_popn,
                        col_types = "i",
@@ -32,10 +37,9 @@ popn_maori <- read_csv("injuries/DPE479901_20230101_021445_73.csv.gz",
                  names_to = c("sex", "age"),
                  names_sep = " ",
                  values_to = "Maori") %>%
-    mutate(age = clean_age(age))
+    mutate(age = reformat_age(age))
 
-
-popn_total <- read_csv("injuries/DPE403903_20230101_021709_80.csv.gz",
+popn_total <- read_csv(.popn_total_raw,
                        skip = 4,
                        col_names = col_names_popn,
                        col_types = "i",
@@ -44,7 +48,7 @@ popn_total <- read_csv("injuries/DPE403903_20230101_021709_80.csv.gz",
                  names_to = c("sex", "age"),
                  names_sep = " ",
                  values_to = "Total") %>%
-    mutate(age = clean_age(age))
+    mutate(age = reformat_age(age))
 
 popn <- inner_join(popn_maori, popn_total, by = c("age", "sex", "year")) %>%
     mutate(`Non Maori` = Total - Maori,
@@ -61,5 +65,5 @@ injuries <- inner_join(injuries_all, popn, by = c("age", "sex", "year", "ethnici
     droplevels()
 
 save(injuries,
-     file = "../data/injuries.rda",
+     file = .out, 
      compress = "bzip2")
