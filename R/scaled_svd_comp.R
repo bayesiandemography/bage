be9abdc6717529de3821ba77d9005ea6e9aa0a9f
johnrbryant <john.r.bryant@gmail.com>
2024-03-14 15:00:30 +1300

    working on scaled_svd_comp

--- R/scaled_svd_comp.R
+++ R/scaled_svd_comp.R
@@ -1,4 +1,7 @@
 
+## WARNING - All these functions will be moved
+## out of 'bage' into their own package
+
 ## HAS_TESTS
 #' Create components needed by a scaled_svd object
 #'
@@ -9,7 +12,6 @@
 #'
 #' `scaled_svd_comp() typically proceeds as follows:
 #' - transform values in matrix `x` (eg take logs)
-#' - center each column of `x`
 #' - carry out a SVD on the transformed version of `x`
 #' - centre and scale the results from the SVD to produce
 #' matrix `matrix` and vector `offset`.
@@ -17,15 +19,15 @@
 #' If 
 #' - \eqn{X} is a the matrix of transformed values
 #' of `x`
-#' - \eqn{A} is the matrix called `matrix`,
-#' - \eqn{b} is the vector called `offset`, and
-#' - \eqn{\beta} is a vector of standard normal variates,
+#' - \eqn{F} is the matrix called `matrix`,
+#' - \eqn{g} is the vector called `offset`, and
+#' - \eqn{\alpha} is a vector of standard normal variates,
 #'
 #' and
 #'
-#' \deqn{v = A \beta + b}
+#' \deqn{\beta = F \alpha + g}
 #'
-#' then \eqn{v} should look like a randomly-selected
+#' then \eqn{\beta} should look like a randomly-selected
 #' column from \eqn{X}.
 #'
 #' Matrix `x` typically has age along the rows,
@@ -39,7 +41,7 @@
 #' The number of components used by the SVD
 #' (and hence the number of columns of the
 #' `tranform` matrix) is governed by
-#' argument `n`. The default is 5.
+#' argument `n`. The default is 10.
 #'
 #' When `scale` is `"log"` or `"logit"`,
 #' `scaled_svd_comp()` converts any `0`s in
@@ -60,23 +62,15 @@
 #' - `matrix`, a numeric matrix
 #' - `offset`, a numeric vector
 #'
-#' @seealso
-#' - [poputils::to_matrix()] can create matrices to
-#' supply to `scaled_svd_comp`
-#' - <name of vignette> has mathematical details
-#' - example of use of `scaled_svd_comp`:
-#' href{https://github.com/bayesiandemography/svd_mortality}{svd_mortality}
-#' 
 #' @examples
 #' x <- matrix(rgamma(n = 50, shape = 1), nrow = 5, ncol = 10)
 #' x
 #' scaled_svd_comp(x, n = 3)
 #' scaled_svd_comp(x, n = 3, transform = "none")
-#' @export
+#' @noRd
 scaled_svd_comp <- function(x,
                             n = 10,
-                            transform = c("log", "logit", "none"),
-                            center = TRUE) {
+                            transform = c("log", "logit", "none")) {
   ## check 'n'
   check_n(n = n,
           nm_n = "n",
@@ -88,32 +82,23 @@ scaled_svd_comp <- function(x,
   check_is_matrix(x, nm_x = "x")
   check_numeric(x, nm_x = "x")
   if (ncol(x) < n)
-    stop(gettextf("number of columns in '%s' [%d] less than '%s' [%d]",
-                  "x",
-                  ncol(x),
-                  "n",
-                  n),
-         call. = FALSE)
+    cli::cli_abort(c("{.arg n} less than number of columns of {.arg x}",
+                     i = "n: {.val {n}}.",
+                     i = "ncol(x): {.val {ncol(x)}}."))
   ## check 'transform'
   transform <- match.arg(transform)
   if (transform %in% c("log", "logit")) {
-    if (any(x < 0))
-      stop(gettextf("'%s' is \"%s\" but '%s' has negative values",
-                    "transform",
-                    transform,
-                    "x"),
-           call. = FALSE)
+    n_neg <- sum(x < 0)
+    if (n_neg > 0)
+      cli::cli_abort(paste("{.arg transform} is {.val {transform}} but {.arg x} has",
+                           "{n_neg} negative value{?s}."))
   }
   if (transform  == "logit") {
-    if (any(x > 1))
-      stop(gettextf("'%s' is \"%s\" but '%s' has values greater than 1",
-                    "transform",
-                    transform,
-                    "x"),
-           call. = FALSE)
+    n_gt_1 <- sum(x > 1)
+    if (n_gt_1 > 0L)
+      cli::cli_abort(paste("{.arg transform} is {.val {transform}} but {.arg x} has",
+                           "{n_gt_1} value{?s} greater than 1."))
   }
-  ## check 'center'
-  check_flag(center)
   ## transform to log or logit scale if necessary
   if (transform == "log") {
     x <- replace_zeros(x)
@@ -122,12 +107,6 @@ scaled_svd_comp <- function(x,
   if (transform == "logit") {
     x <- replace_zeros_ones(x)
     x <- log(1 / (1 - x))
-  }
-  ## center, if required
-  if (center) {
-    col_means <- colMeans(x)
-    col_means <- rep(col_means, each = nrow(x))
-    x <- x - col_means
   }
   ## apply svd
   svd <- svd(x = x,
@@ -226,3 +205,164 @@ replace_zeros_ones <- function(x) {
     x
 }
 
+
+## HAS_TESTS
+#' Convert a Zipped HMD into a Combined Data Frame
+#'
+#' Zipped HMD file obtained from
+#' https://www.mortality.org/Data/ZippedDataFiles
+#' 
+#' @param file Name of zipped HMD file
+#'
+#' @returns A tibble
+#'
+#' @noRd
+hmd_unzip <- function(file) {
+  dirs <- c("lt_both/bltper_1x1",
+            "lt_both/bltper_5x1",
+            "lt_female/fltper_1x1",
+            "lt_female/fltper_5x1",
+            "lt_male/mltper_1x1",
+            "lt_male/mltper_5x1")
+  tmp_dir <- tempdir()
+  on.exit(unlink(tmp_dir, recursive = TRUE))
+  utils::unzip(file, exdir = tmp_dir)
+  get_data <- function(dir) {
+    country_code <- sub("([A-Z]+)\\..*", "\\1", list.files(dir))
+    sex <- sub(".*lt_([a-z]+).*", "\\1", dir)
+    type_age <- sub(".*ltper_([1x5]+).*", "\\1", dir)
+    files <- list.files(dir, full.names = TRUE)
+    ans <- lapply(files,
+                  read.table,
+                  header = TRUE,
+                  colClasses = c("integer",
+                                 "character",
+                                 "double",
+                                 rep("NULL", 4L),
+                                 "integer",
+                                 rep("NULL", 2L)),
+                  na.strings = ".",
+                  skip = 1)
+    add_nm <- function(x, nm) {
+      x[["country"]] <- nm
+      x <- x[c(length(x), seq_len(length(x) - 1L))]
+      x
+    }
+    ans <- .mapply(add_nm,
+                   dots = list(x = ans,
+                               nm = country_code),
+                   MoreArgs = list())
+    ans <- do.call(rbind, ans)
+    ans$sex <- sex
+    ans$type_age <- type_age
+    ans
+  }
+  dirs_unpacked <- file.path(tmp_dir,
+                             sub("\\.zip$", "", basename(file)),
+                             dirs)
+  ans <- lapply(dirs_unpacked, get_data)
+  ans <- do.call(rbind, ans)
+  ans
+}
+
+
+## HAS_TESTS
+#' Tidy Raw HMD data
+#'
+#' Steps:
+#' - rename "Age" to "age"
+#' - rename "Year" to "time"
+#' - change levels for 'sex' variable to "Total", "Female", "Male"
+#' - exclude any country-time combinations that have
+#'   NAs for 'mx' or 'Lx'
+#'
+#' @param data A data fame with raw HMD data
+#'
+#' @returns A cleaned data frame
+#'
+#' @noRd
+hmd_tidy_data <- function(data) {
+  names(data)[[match("Age", names(data))]] <- "age"
+  names(data)[[match("Year", names(data))]] <- "time"
+  data$sex <- factor(data$sex,
+                     levels = c("both", "female", "male"),
+                     labels = c("Total", "Female", "Male"))
+  data$type_age <- factor(data$type_age,
+                          levels = c("1x1", "five", "5x1"), ## "5x1" splits 0-4
+                          labels = c("single", "five", "lt"))
+  has_na <- aggregate(x = data.frame(has_na = is.na(data$mx) | is.na(data$Lx)),
+                      by = data[c("country", "time", "type_age")],
+                      FUN = any)
+  data <- merge(x = data,
+                y = has_na,
+                by = c("country", "time", "type_age"),
+                sort = FALSE)
+  data <- data[!data$has_na, ]
+  data <- data[-match("has_na", names(data))]
+  data
+}
+
+
+
+#' Create Age Group "five" and Add to 'data'
+#'
+#' Create age group "five" by combining
+#' age groups 0 and 1-4 in "lt"
+#'
+#' @param data A data frame
+#'
+#' @returns Modifed version of 'data'
+#'
+#' @noRd
+hmd_add_age_five <- function(data) {
+  five <- data[data$type_age == "lt", ]
+  five$type_age <- "five"
+  five_04 <- five[five$age %in% c("0", "1-4"), ]
+  five_04 <- aggregate_mx_Lx(five_04)
+  five_04$dx <- five_04$mx * five_04$Lx
+  five_04 <- aggregate(five_04[c("Lx", "dx")],
+                       five[c("sex", "country", "time", "type_age")]
+                       sum)
+  five_04$mx <- five_04$dx / five_04$Lx
+  five_04 <- five_04[-match("dx", names(five_04))]
+  five_04$age <- "0-4"
+  five_5pl <- five[!(five$age %in% c("0", "1-4")), ]
+  five <- rbind(five_04, five_5pl)
+  data <- rbind(data, five)
+}
+
+
+#' Take Weighted Mean of 'mx' and Add Up 'Lx'
+#'
+#' Aggregation happens within categories
+#' defined by non-mx, non-Lx variables.
+#'
+#' Case where Lx all 0 or all NA dealt with.
+#'
+#' @param data A data frame that includes
+#' columns 'mx' and 'Lx'
+#'
+#' @returns A data frame
+#'
+#' @noRd
+aggregate_mx_Lx <- function(data) {
+  nms <- names(data)
+  nms_x <- c("mx", "Lx")
+  nms_by <- setdiff(nms, c("mx", "Lx"))
+  data <- vctrs::vec_split(x = data[nms_x], by = data[nms_f])
+  fill_Lx <- function(x) {
+    if (sum(x$Lx, na.rm = TRUE) == 0)
+      x$Lx <- 1
+    x
+  }
+  val <- lapply(data$val, fill_Lx)
+  sum_or_mean <- function(x)
+    tibble::tibble(mx = weighted.mean(x = x$mx, w = x$Lx),
+                   Lx = sum(x$Lx))
+  val <- lapply(val, sum_or_mean)
+  val <- vctrs::vec_rbind(!!!val, .name_repair = "universal_quiet")
+  vctrs::vec_rbind(data$key, val)
+}
+
+  
+  
--- tests/testthat/test-scaled_svd_comp.R
+++ tests/testthat/test-scaled_svd_comp.R
@@ -45,6 +45,43 @@ test_that("'scaled_svd_comp' performs correctly with valid inputs - none", {
     expect_false(any(sapply(ans, anyNA)))
 })
 
+test_that("'scaled_svd_comp' gives expected error when n < ncol(x)", {
+    set.seed(0)
+    x <- expand.grid(age = 0:5,
+                     sex = c("F", "M", "T"),
+                     year = 2000:2001,
+                     country = c("a", "b"))
+    x$value <- rnorm(nrow(x))
+    x <- poputils::to_matrix(x, rows = c(age, sex), cols = c(year, country), measure = value)
+    expect_error(scaled_svd_comp(x = x, transform = "none"),
+                 "`n` less than number of columns of `x`.")
+})
+
+test_that("'scaled_svd_comp' gives expected error when negative values", {
+    x <- expand.grid(age = 0:5,
+                     sex = c("F", "M", "T"),
+                     year = 2000:2001,
+                     country = c("a", "b"))
+    x$value <- c(-2, -2, rep(0.1, times = 70))
+    x <- poputils::to_matrix(x, rows = age, cols = c(sex, year, country), measure = value)
+    expect_error(scaled_svd_comp(x = x, transform = "log"),
+                 "`transform` is \"log\" but `x` has 2 negative values")
+})
+
+test_that("'scaled_svd_comp' gives expected error when negative values", {
+    x <- expand.grid(age = 0:5,
+                     sex = c("F", "M", "T"),
+                     year = 2000:2001,
+                     country = c("a", "b"))
+    x$value <- c(2, 2, rep(0.1, times = 70))
+    x <- poputils::to_matrix(x, rows = age, cols = c(sex, year, country), measure = value)
+    expect_error(scaled_svd_comp(x = x, transform = "logit"),
+                 "`transform` is \"logit\" but `x` has 2 values greater than 1.")
+})
+
+
+
+
 
 ## 'replace_zeros' ------------------------------------------------------------
 
@@ -71,8 +108,45 @@ test_that("'replace_zeros_ones' performs correctly with valid inputs", {
 
 
     
-    
-    
+## 'hmd_unzip' ----------------------------------------------------------------
+
+## ## code to make test file:
+## for (dn1 in list.files("data_for_tests/hmd_statistics_20240226", full = TRUE)) {
+##   for (dn2 in list.files(dn1, full = TRUE)) {
+##     for (fn in list.files(dn2, full = TRUE)) {
+##       lines <- readLines(fn, n = 10)
+##       writeLines(lines, con = fn)
+##     }
+##   }
+## }  
+
+test_that("'hmd_unzip' works with valid inputs", {
+  fn <- file.path("data_for_tests", "hmd_statistics_test.zip")
+  ans <- hmd_unzip(fn)
+  expect_setequal(names(ans),
+                  c("country", "Year", "Age", "mx", "Lx", "sex", "type_age"))
+})
+
+
+## 'hmd_tidy_data' ------------------------------------------------------------
+
+test_that("'hmd_tidy_data' works", {
+  data <- tibble::tribble(~Age, ~Year, ~sex,     ~country, ~type_age, ~mx, ~Lx,
+                          0,    2001,  "both",   "a",      "1x1",     0.1, 2,
+                          1,    2002,  "female", "b",      "5x1",     0.1, 2,
+                          2,    2003,  "male",   "c",      "1x1",     0.1, 2,
+                          3,    2004,  "both",   "d",      "1x1",     NA,  0)
+  ans_obtained <- hmd_tidy_data(data)
+  expect_setequal(names(ans_obtained),
+                  c("age", "time", "sex", "country", "type_age", "mx", "Lx"))
+  expect_identical(nrow(ans_obtained), 3L)
+  expect_identical(ans_obtained$sex,
+                   factor(c("Total", "Female", "Male"),
+                          levels = c("Total", "Female", "Male")))
+  expect_identical(ans_obtained$type_age,
+                   factor(c("single", "lt", "single"),
+                          levels = c("single", "five", "lt")))
+})
 
 
 
