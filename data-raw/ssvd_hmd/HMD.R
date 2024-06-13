8c18d8d4ca091921fbd371bc596fa2322c68a04c
John Bryant <john.r.bryant@gmail.com>
2024-06-13 19:33:09 +1200

    moved hmd to bssvd

--- data-raw/ssvd_hmd/HMD.R
+++ data-raw/ssvd_hmd/HMD.R
@@ -1,11 +1,13 @@
 
 library(command)
+library(bssvd)
 library(bage)
 
-cmd_assign(.hmd = "svd_hmd/hmd_statistics_20240226.zip",
+cmd_assign(.hmd = "ssvd_hmd/hmd_statistics_20240226.zip",
            .out = "../data/HMD.rda")
 
-HMD <- ssvd_hmd(.hmd)
+data <- data_ssvd_hmd(.hmd)
+HMD <- ssvd(data)
 
 save(HMD, file = .out, compress = "bzip2")
 
