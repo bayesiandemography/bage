9e9b523a5fd98a91091dfaeb164c66db09aee21e
John Bryant <john.r.bryant@gmail.com>
2024-09-16 16:35:14 +0700

    modified sticker

--- data-raw/sticker/sticker.R
+++ data-raw/sticker/sticker.R
@@ -1,20 +1,16 @@
 
 library(hexSticker)
-library(ggplot2)
 
-data <- data.frame(x = 1, y = 1)
-
-p <- ggplot(data, aes(x = x, y = y)) +
-    geom_point() +
-    theme_void()
-
-sticker(subplot = p,
+sticker("background.png",
         package = "bage",
-        p_size = 32,
+        p_size = 40,
         p_y = 1,
-        p_color = "brown",
-        s_width = 0,
+        p_color = "beige",
+        p_family = "Aller",
+        s_x = 1,
+        s_y = 1,            
+        s_width = 0.6,        
         h_fill = "beige",
+        white_around_sticker = FALSE,
         h_color = "brown",
         filename = "sticker.png")
-
