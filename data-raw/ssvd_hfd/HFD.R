c297ad4ccdd8fe3a7776b06ef87c5b55d2243828
John Bryant <john.r.bryant@gmail.com>
2024-09-24 08:03:37 +0700

    added HFD

--- data-raw/ssvd_hfd/HFD.R
+++ data-raw/ssvd_hfd/HFD.R
@@ -2,11 +2,12 @@
 library(command)
 library(bssvd)
 library(bage)
+library(readr)
 
 cmd_assign(.asfr = "ssvd_hfd/asfrRR.txt.zip",
            .out = "../data/HFD.rda")
 
-asfr <- readr::read_table(.asfr, skip = 2)
+asfr <- read_table(.asfr, skip = 2)
 
 data <- data_ssvd_hfd(asfr, n_comp = 5)
 HFD <- bage:::ssvd(data)
