8dc237148b14ce8152b7eaee6b18f2ad9bc7b233
John Bryant <john.r.bryant@gmail.com>
2025-08-03 15:22:55 +0700

    working on datamod section of vig2

--- R/bage_confidential-methods.R
+++ R/bage_confidential-methods.R
@@ -3,9 +3,11 @@
 
 #' Generate Confidentialized Values of Observed Outcome Variable
 #'
+#' Generate confidentialized values from observed values.
+#' The observed values may be subject to measurement error.
+#'
 #' @param confidential Object of class 'bage_confidential'
 #' @param outcome_obs Rvec with observed values for outcome
-#' (ie may be subject to measurement error)
 #'
 #' @returns An rvec
 #'
@@ -20,6 +22,81 @@ draw_outcome_confidential.bage_confidential_rr3 <- function(confidential,
                                                             outcome_obs) {
   poputils::rr3(outcome_obs)
 } 
+
+
+## 'draw_outcome_obs_given_conf' ----------------------------------------------
+
+#' Draw Values for Observed Outcome, Given Values for
+#' Confidentialized Outcome
+#'
+#' @param confidential Object of class 'bage_confidential'
+#' @param outcome_obs Rvec with observed values for outcome
+#' and 
+#' (ie may be subject to measurement error)
+#'
+#' @returns An rvec
+#'
+#' @noRd
+draw_outcome_confidential <- function(confidential,
+                                      outcome_obs) {
+  UseMethod("draw_outcome_confidential")
+}
+
+## NO_TESTS
+#' @export
+draw_outcome_true.bage_datamod_outcome_rr3 <- function(datamod,
+                                                            nm_distn,
+                                                            outcome_obs,
+                                                            fitted,
+                                                            expected,
+                                                            disp,
+                                                            offset) {
+  n_val <- length(outcome_obs)
+  has_disp <- !is.null(disp)
+  if (has_disp) {
+    n_draw <- rvec::n_draw(expected)
+    expected <- as.matrix(expected)
+    disp <- matrix(as.numeric(disp), nrow = n_val, ncol = n_draw, byrow = TRUE)
+    offset <- matrix(offset, nrow = n_val, ncol = n_draw)
+    if (nm_distn == "pois") {
+      nm_dist_detailed <- "nbinom"
+      shape <- 1 / disp
+      rate <- 1 / (expected * offset * disp)
+      args <- list(shape = shape, rate = rate)
+    }
+    else if (nm_distn == "binom") {
+      nm <- "betabinom"
+      shape <- 1 / disp
+      rate <- 1 / (expected * offset * disp)
+      args <- list(size = offset, shape = shape, rate = rate)
+    }
+    else
+      cli::cli_abort("Internal error: Invalid value for {.var nm_distn}.")
+  }
+  else {
+    n_draw <- rvec::n_draw(fitted)
+    fitted <- as.matrix(fitted)
+    offset <- matrix(offset, nrow = n_val, ncol = n_draw)
+    if (nm_distn == "pois") {
+      nm_dist_detailed <- "pois"
+      lambda <- fitted * offset
+      args <- list(lambda = lambda)
+    }
+    else if (nm_distn == "binom") {
+      nm <- "binom"
+      args <- list(size = offset, prob = fitted)
+    }
+    else
+      cli::cli_abort("Internal error: Invalid value for {.var nm_distn}.")
+  }
+  args <- lapply(seq_along(n_val),
+                 function(i) lapply(args, function(j) arg[i, ]))
+  draw_vals_outcome_true_rr3(nm_distn_detailed = nm_dist_detailed,
+                             outcome_obs = outcome_obs,
+                             args = args,
+                             offset = offset,
+                             n_draw = n_draw)
+}                            
 
 
 ## 'make_i_lik_part' --------------------------------------------------
--- new.R
+++ new.R
@@ -7,53 +7,59 @@ draw_vals_augment_fitted.bage_mod <- function(mod) {
   offset <- mod$offset
   seed_augment <- mod$seed_augment
   confidential <- mod$confidential
-  datamod_outcome <- mod$datamod_outcome
-  datamod_offset <- mod$datamod_offset
+  datamod <- mod$datamod
   nm_distn <- nm_distn(mod)
   disp <- get_disp(mod)
+  disp_datamod <- get_disp_datamod(mod)
   inv_transform <- get_fun_inv_transform(mod)
   ## prepare inputs
   linpred <- make_linpred_from_stored_draws(mod = mod, point = FALSE)
   expected <- inv_transform(linpred)
   has_confidential <- !is.null(confidential)
-  has_datamod_outcome <- !is.null(datamod_outcome)
+  has_datamod <- !is.null(datamod)
+  has_datamod_outcome <- inherits(datamod, "bage_datamod_outcome")
+  has_datamod_offset <- inherits(datamod, "bage_datamod_offset")
   has_missing_outcome <- anyNA(outcome)
-  has_datamod_offset <- !is.null(datamod_offset)
   has_disp <- has_disp(mod)
   ## prepare seeds
   seed_restore <- make_seed() ## create randomly-generated seed
   set.seed(seed_augment) ## set pre-determined seed
   ## draw values for outcome and offset, where necessary
   if (has_confidential) {
-    outcome <- draw_outcome_obs_given_conf(confidential = confidential,
+    if (has_datamod) {
+      expected_obs <- make_expected_obs(datamod = datamod,
+                                        expected = expected,
+                                        disp_datamod = disp_datamod)
+    }
+    else
+      expected_obs <- expected
+    outcome_obs <- draw_outcome_obs_given_conf(confidential = confidential,
+                                               nm_distn = nm_distn,
+                                               outcome_conf = outcome,
+                                               offset = offset,
+                                               expected_obs = expected_obs,
+                                               disp = disp)
+  }
+  if (has_datamod) {
+    outcome <- draw_outcome_true_given_obs(datamod = datamod,
                                            outcome = outcome,
                                            offset = offset,
                                            nm_distn = nm_distn,
                                            expected = expected,
                                            disp = disp)
-  }
-  if (has_datamod_outcome) {
-    outcome <- draw_outcome_true_given_obs(datamod = datamod_outcome,
-                                           outcome = outcome,
-                                           offset = offset,
-                                           nm_distn = nm_distn,
-                                           expected = expected,
-                                           disp = disp)
-  }
-  if (has_missing_outcome) {
-    outcome <- impute_outcome_true(outcome = outcome,
-                                   offset = offset,
-                                   nm_distn = nm_distn,
-                                   expected = expected,
-                                   disp = disp)
-  }
-  if (has_datamod_offset) {
     offset <- draw_offset_true_given_obs(datamod = datamod_outcome,
+                                         nm_distn = nm_distn,
                                          outcome = outcome,
                                          offset = offset,
-                                         nm_distn = nm_distn,
                                          expected = expected,
                                          disp = disp)
+  }
+  if (has_missing_outcome) {
+    outcome <- impute_outcome_true(nm_distn = nm_distn,
+                                   outcome = outcome,
+                                   offset = offset,
+                                   expected = expected,
+                                   disp = disp)
   }
   ## derive '.fitted' and, in models with dispersion, '.expected'
   ans <- mod$data
@@ -103,34 +109,35 @@ draw_vals_augment_fitted.bage_mod_norm <- function(mod) {
   offset <- mod$offset
   seed_augment <- mod$seed_augment
   datamod_outcome <- mod$datamod_outcome
-  outcome_sd <- mod$outcome_sd
-  offset_mean <- mod$offset_mean
   nm_distn <- nm_distn(mod)
   disp <- get_disp(mod)
   scale_linpred <- get_fun_scale_linpred(mod)
   scale_disp <- get_fun_scale_disp(mod)
+  scale_offset <- get_fun_scale_offset(mod)
   ## prepare inputs
   linpred <- make_linpred_from_stored_draws(mod = mod, point = FALSE)
+  disp <- get_disp(mod)
   has_datamod_outcome <- !is.null(datamod_outcome)
   has_missing_outcome <- anyNA(outcome)
-  expected_scaled <- scale_linpred(expected)
+  expected_scaled <- scale_linpred(linpred)
   disp_scaled <- scale_disp(disp)
+  offset_scaled <- scale_offset(offset)
   ## prepare seeds
   seed_restore <- make_seed() ## create randomly-generated seed
   set.seed(seed_augment) ## set pre-determined seed
   ## draw values for outcome, where necessary
   if (has_datamod_outcome) {
     outcome <- draw_outcome_true_given_obs(datamod = datamod_outcome,
-                                           outcome = outcome,
-                                           offset = offset,
                                            nm_distn = nm_distn,
+                                           outcome = outcome,
+                                           offset = offset_scaled,
                                            expected = expected_scaled,
                                            disp = disp_scaled)
   }
   if (has_missing_outcome) {
-    outcome <- impute_outcome_true(outcome = outcome,
-                                   offset = offset,
-                                   nm_distn = nm_distn,
+    outcome <- impute_outcome_true(nm_distn = nm_distn,
+                                   outcome = outcome,
+                                   offset = offset_scaled,
                                    expected = expected_scaled,
                                    disp = disp_scaled)
   }
@@ -251,56 +258,61 @@ draw_vals_augment_unfitted.bage_mod_norm <- function(mod) {
   offset <- mod$offset
   seed_augment <- mod$seed_augment
   datamod_outcome <- mod$datamod_outcome
-  outcome_sd <- mod$outcome_sd
-  offset_mean <- mod$offset_mean
   dimnames_terms <- mod$dimnames_terms
   n_draw <- mod$n_draw
   nm_distn <- nm_distn(mod)
   disp <- get_disp(mod)
-  scale_linpred <- get_fun_scale_linpred(mod)
+  orig_scale_linpred <- get_fun_orig_scale_linpred(mod)
+  orig_scale_disp <- get_fun_orig_scale_disp(mod)
+  orig_scale_offset <- get_fun_orig_scale_offset(mod)
   nm_outcome_data <- get_nm_outcome_data(mod)
   ## prepare inputs
+  has_confidential <- !is.null(confidential)
   has_datamod_outcome <- !is.null(datamod_outcome)
-  offset_scaled <- offset_mean * offset
+  ans <- mod$data
   ## prepare seeds
   seed_restore <- make_seed() ## create randomly-generated seed
   set.seed(seed_augment) ## set pre-determined seed
   ## draw values for components
   components <- draw_vals_components_unfitted(mod = mod,
                                               n_sim = n_draw)
-  ## derive expected values
+  ## derive linpred
   linpred <- make_linpred_from_components(mod = mod,
                                           components = components,
                                           data = data,
                                           dimnames_terms = dimnames_terms)
-  ## obtain values for 'fitted' and 'disp'
-  vals_fitted <- scale_linpred(vals_linpred)
+  ## obtain values for 'fitted', 'disp', and 'offset' on original scale
+  fitted_orig_scale <- orig_scale_linpred(linpred)
   is_disp <- vals_components$component == "disp"
   disp <- vals_components$.fitted[is_disp]
-  disp_scaled <- sqrt(offset_mean) * outcome_sd * vals_disp
+  disp_orig_scale <- orig_scale_disp(disp)
+  offset_orig_scale <- orig_scale_offset(offset)
   ## obtain values for true outcome
-  ## NEED TO DEAL WITH SCALING
-  outcome <- draw_outcome_true(offset = offset,
-                               nm_distn = nm_distn,
-                               fitted = fitted,
-                               disp = disp)
-  ## draw values for observed outcome, where necessary
-  if (has_datamod_outcome)
-    outcome_obs <- draw_outcome_obs(datamod_outcome = datamod_outcome,
-                                    outcome_true = outcome)
+  outcome <- draw_outcome_true(nm_distn = nm_distn,
+                               offset = offset_orig_scale,
+                               fitted = fitted_orig_scale,
+                               disp = disp_orig_scale)
+  ## create modified value of outcome,
+  ## where necessary, and record outcome(s)
+  has_modified_outcome <- has_datamod_outcome || has_confidential
+  if (has_modified_outcome) {
+    outcome_true <- outcome
+    if (has_datamod_outcome)
+      outcome <- draw_outcome_obs(datamod_outcome = datamod_outcome,
+                                  outcome_true = outcome)
+    if (has_confidential)
+      outcome <- draw_outcome_confidential(datamod_outcome = datamod_outcome,
+                                           outcome_obs = outcome)
+    ans[[nm_outcome_data]] <- outcome
+    nm_outcome_data_true <- paste0(".", nm_outcome_data)
+    ans[[nm_outcome_data_true]] <- outcome_true
+  }
+  else
+    ans[[nm_outcome_data]] <- outcome
   ## restore seed
   set.seed(seed_restore) ## set randomly-generated seed, to restore randomness
   ## assemble and return answer
-  ans <- mod$data
-  if (has_datamod_outcome) {
-    ans[[nm_outcome_data]] <- vals_outcome_obs
-    nm_outcome_data_true <- paste0(".", nm_outcome_data)
-    ans[[nm_outcome_data_true]] <- vals_outcome_true
-  }
-  else {
-    ans[[nm_outcome_data]] <- vals_outcome_true
-  }
-  ans$.fitted <- vals_fitted
+  ans$.fitted <- fitted_orig_scale
   ans
 }
 
@@ -309,213 +321,75 @@ draw_vals_augment_unfitted.bage_mod_norm <- function(mod) {
 
 ## HAS_TESTS
 #' @export
-draw_vals_outcome_true.bage_datamod_outcome_rr3 <- function(datamod,
-                                                            nm_distn,
-                                                            outcome_obs,
-                                                            fitted,
-                                                            expected,
-                                                            disp,
-                                                            offset) {
-  n_val <- length(outcome_obs)
-  has_disp <- !is.null(disp)
+draw_outcome_obs_given_conf.bage_confidential_rr3 <- function(confidential,
+                                                              nm_distn,
+                                                              outcome_conf,
+                                                              offset
+                                                              expected_obs,
+                                                              disp) {
+  n_val <- length(expected_obs)
+  n_draw <- rvec::n_draw(expected_obs)
+  n_outcome <- 5L
+  expected_obs <- as.numeric(expected_obs)
+  expected_obs <- rep(expected_obs, each = n_outcome)
+  s_outcome <- seq.int(from = -2L, to = 2L)
+  outcome_true <- outer(s_outcome, outcome_conf, FUN = "+")
+  outcome_true <- rep(outcome_true, times = n_draw)
   if (has_disp) {
-    n_draw <- rvec::n_draw(expected)
-    expected <- as.matrix(expected)
-    disp <- matrix(as.numeric(disp), nrow = n_val, ncol = n_draw, byrow = TRUE)
-    offset <- matrix(offset, nrow = n_val, ncol = n_draw)
+    disp <- as.numeric(disp)
+    disp <- rep(disp, each = n_outcome * n_val)
     if (nm_distn == "pois") {
-      nm_dist_detailed <- "nbinom"
       shape <- 1 / disp
-      rate <- 1 / (expected * offset * disp)
-      args <- list(shape = shape, rate = rate)
+      rate <- 1 / (expected_obs * offset * disp)
+      prob_prior <- dnbinom(x = outcome_true,
+                           shape = shape,
+                           rate = rate)
     }
     else if (nm_distn == "binom") {
-      nm <- "betabinom"
-      shape <- 1 / disp
-      rate <- 1 / (expected * offset * disp)
-      args <- list(size = offset, shape = shape, rate = rate)
+      
+      shape1 <- expected_obs / disp
+      shape2 <- (1 - expected_obs) / disp
+      prob_prior <- dbetabinom(x = outcome_true,
+                              size = offset,
+                              shape1 = shape1,
+                              shape2 = shape2)
     }
     else
       cli::cli_abort("Internal error: Invalid value for {.var nm_distn}.")
   }
   else {
-    n_draw <- rvec::n_draw(fitted)
-    fitted <- as.matrix(fitted)
-    offset <- matrix(offset, nrow = n_val, ncol = n_draw)
     if (nm_distn == "pois") {
-      nm_dist_detailed <- "pois"
-      lambda <- fitted * offset
-      args <- list(lambda = lambda)
+      lambda <- expected_obs * offset
+      prob_prior <- dpois(x = outcome_true,
+                         lambda = lambda)
     }
     else if (nm_distn == "binom") {
-      nm <- "binom"
-      args <- list(size = offset, prob = fitted)
+      prob_prior <- dbinom(x = outcome_true,
+                          size = offset,
+                          prob = expected_obs)
     }
     else
       cli::cli_abort("Internal error: Invalid value for {.var nm_distn}.")
   }
-  args <- lapply(seq_along(n_val),
-                 function(i) lapply(args, function(j) arg[i, ]))
-  draw_vals_outcome_true_rr3(nm_distn_detailed = nm_dist_detailed,
-                             outcome_obs = outcome_obs,
-                             args = args,
-                             offset = offset,
-                             n_draw = n_draw)
-}                            
-
-
-#' Draw Values for Outcome Variable When Value Not Supplied
-#' 
-draw_vals_outcome_no_obs <- function(nm_distn_detailed,
-                                     args,
-                                     offset) {
-  if (nm_distn_detailed == "pois") {
-    lambda <- args$lambda
-    stats::rpois(n = length(lambda),
-                 lambda = lambda)
-  }
-  else if (nm_distn_detailed == "nbinom") {
-    shape <- args$shape
-    rate <- args$rate
-    stats::rnbinom(n = length(shape),
-                   shape = shape,
-                   rate = rate)
-  }
-  else if (nm_distn_detailed == "binom") {
-    size <- args$size
-    prob <- args$prob
-    stats::rbinom(n = length(size),
-                  size = size,
-                  prob = prob)
-  }
-  else if (nm_distn_detailed == "betabinom") {
-    size <- args$size
-    shape1 <- args$shape1
-    shape2 <- args$shape2
-    stats::rbetabinom(n = length(size),
-                      size = size,
-                      shape1 = shape1,
-                      shape2 = shape2)
-  }
-  else
-    cli::cli_abort(paste("Internal error: Invalid value for",
-                         "{.var nm_distn_detailed}."))
-}
-
-
-draw_vals_outcome_true_rr3 <- function(nm_distn_detailed,
-                                       outcome_obs,
-                                       args,
-                                       offset,
-                                       n_draw) {
-  n_val <- length(outcome_obs)
-  ans <- matrix(NA_integer_, nrow = n_val, ncol = n_draw)
-  for (i_val in seq_len(n_val)) {
-    outcome_obs_i <- outcome_obs[[i_val]]
-    offset_i <- offset[[i_val]]
-    has_outcome <- is.na(outcome_obs_i)
-    has_offset <- is.na(offset_i)
-    args_i <- args[[i_val]]
-    if (has_offset) {
-      if (has_outcome) {
-        ans[i_val, ] <- draw_vals_outcome_no_outcome(nm_distn_detailed = nm_dist_detailed,
-                                                     args = args_i,
-                                                     offset = offset_i)
-      }
-      else {
-        ans[i_val, ] <- draw_vals_outcome_has_outcome_obs_rr3(nm_distn_detailed = nm_dist_detailed,
-                                                              n_draw = n_draw,
-                                                              args = args_i,
-                                                              outcome_obs = outcome_obs_i,
-                                                              offset = offset_i)
-      }
-    }
-  }
+  prob_prior[outcome < 0L] <- 0
+  prob_obs_given_true <- c(1/3, 2/3, 1, 2/3, 1/3)
+  prob_true <- prob_prob_obs_given_true * prior_prior
+  prob_true <- matrix(prob_true,
+                      nrow = n_outcome,
+                      ncol = n_val * n_draw)
+  outcome_true <- matrix(outcome_true,
+                         nrow = n_outcome,
+                         ncol = n_val * n_draw)
+  i <- apply(prob_true, margin = 2L, sample, size = 1L)
+  j <- seq_len(n_val * n_draw)
+  ans <- outcome_true[cbind(i, j)]
+  ans <- matrix(nrow = n_val, ncol = n_draw)
   ans <- rvec::rvec(ans)
-  ans
-}
-
-
-
-draw_vals_outcome_has_obs_rr3 <- function(nm_distn_detailed,
-                                          n_draw,
-                                          args,
-                                          observed_obs,
-                                          offset) {
-  is_obs_zero <- observed_obs == 0L
-  if (is_obs_zero) {
-    outcome_true <- 0:2
-    prob_obs_given_true <- c(1, 2/3, 1/3)
-  }
-  else {
-    outcome_true <- seq.int(from = observed_obs - 2L, to = observed_obs + 2L)
-    prob_obs_given_true <- c(1/3, 2/3, 1, 2/3, 1/3)
-  }
-  prob_obs_true <- make_prob_obs_true(nm_distn_detailed = nm_distn_detailed,
-                                      n_draw = n_draw,
-                                      args = args,
-                                      observed_obs = observed_obs,
-                                      offset = offset,
-                                      outcome_true = outcome_true,
-                                      prob_obs_given_true = prob_obs_given_true)
-  ans <- integer(length = n_draw)
-  for (i_draw in seq_len(n_draw)) {
-    ans[[i]] <- sample(x = outcome_true,
-                       size = 1L,
-                       prob = prob_obs_true[, i_draw])
-  }
-  ans
 }
   
 
+  
 
-## calculate 'prob_obs_true', the
-## (unnormalised) probability of
-## each possible value of true outcome
-make_prob_obs_true <- function(nm_distn_detailed,
-                               n_draw,
-                               args,
-                               observed,
-                               offset,
-                               outcome_true,
-                               prob_obs_given_true) {
-  n_poss <- length(outcome_true)
-  outcome_true <- rep.int(outcome_true, times = n_draw)
-  prob_obs_given_true <- rep.int(prob_obs_given_true, times = n_draw)
-  if (nm_distn_detailed == "pois") {
-    lambda <- rep(args$lambda, each = n_poss)
-    prob_prior <- stats::rpois(n = length(lambda),
-                               lambda = lambda)
-  }
-  else if (nm_distn_detailed == "nbinom") {
-    shape <- rep(args$shape, each = n_poss)
-    rate <- repo(args$rate, each = n_poss)
-    prob_prior <- stats::rnbinom(n = length(shape),
-                                 shape = shape,
-                                 rate = rate)
-  }
-  else if (nm_distn_detailed == "binom") {
-    size <- rep(args$size, each = n_poss)
-    prob <- rep(args$prob, each = n_poss)
-    prob_prior <- stats::rbinom(n = length(size),
-                                size = size,
-                                prob = prob)
-  }
-  else if (nm_distn_detailed == "betabinom") {
-    size <- args$size
-    shape1 <- args$shape1
-    shape2 <- args$shape2
-    prob_prior <- stats::rbetabinom(n = length(size),
-                                    size = size,
-                                    shape1 = shape1,
-                                    shape2 = shape2)
-  }
-  else
-    cli::cli_abort("Internal error: Invalid value for {.var nm_distn_detailed}.")
-  ans <- prob_obs_given_true * prob_prior
-  ans <- matrix(ans, nrow = n_poss, ncol = n_draw)
-  ans  
-}  
   
   
     
