be0c816b340072864be24bbebd848993cf7a8959
John Bryant <john.r.bryant@gmail.com>
2025-09-15 07:03:47 +0700

    working on datamodel documentation

--- R/datamods.R
+++ R/datamods.R
@@ -9,12 +9,12 @@
 #'
 #' @details
 #'
-#' | **Function**        | **Assumptions about measurement error**         | **pois** | **binom** | **norm** |
+#' | **Function**        | **Assumptions about measurement error**         | **Poisson** | **Binomial** | **Normal** |
 #' |---------------------|------------------------------------|----------|-----------|----------|
 #' | [set_datamod_miscount()] | Reported outcome has undercount and overcount | Yes | No | No |
 #' | [set_datamod_undercount()] | Reported outcome has undercount | Yes | Yes | No |
 #' | [set_datamod_overcount()] | Reported outcome has overcount | Yes | No | No |
-#' | [set_datamod_noise()] | Reported outcome unbiased, but with measurement errors in both directions | No | No | Yes |
+#' | [set_datamod_noise()] | Reported outcome unbiased, but with measurement errors in both directions | Yes* | No | Yes |
 #' | [set_datamod_exposure()] | Reported exposure unbiased, but with measurement errors in both directions | Yes* | No | No |
 #'
 #' *Models with no dispersion term for rates.
